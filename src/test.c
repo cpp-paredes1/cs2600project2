d407006b3f3337554fd33b26e39815f3086bce8e
Matthew Finerty <mfinerty@cpp.edu>
2022-11-30 13:24:19 -0800

    Create test.c for unit tests

---
+++ src/test.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int printTest(char* text, int result) {
+    printf("%s: %s\n", text, result ? "PASS" : "FAIL");
+    return result;
+}
+
+// Call this function from main if first argument is "test"
+int test() {
+    printf("Performing unit tests...\n");
+    printf("---------------\n");
+    int trueTest = printTest("This should pass", 1 == 1);
+    int falseTest = printTest("This should fail", 1 == 2);
+
+    int result = trueTest && falseTest;
+    return printTest("\nOverall test", result);
+}
