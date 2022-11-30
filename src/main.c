82dbfc4fa5c496dd22f30726e261d263b1d286e4
Kyle Long <kjlong@cpp.edu>
2022-11-29 18:10:30 -0800

    Changed the methods in CarExpense so that each day, the parking and taxis can be different. Main also includes these methods

--- src/main.c
+++ src/main.c
@@ -7,22 +7,24 @@ int main() {
 	// Write program template in here
 	
 	//Ask for:
-		//# of days
+		//# of days (check)
 	int daysNum = integerInputPositive("Input number of days spent on trip\n", 0);
 	printf("Number of days: %d\n", daysNum);
-		//Time of departure
+		//Time of departure (check)
 	int *departTime = timeInput("Enter the time of your departure\n");
 	printf("Departure time: %d:%d\n", departTime[0], departTime[1]);
-		//Time of arrival
+		//Time of arrival (check)
 	int *arriveTime = timeInput("Enter the time of your arrival\n");
 	printf("Departure time: %d:%d\n", arriveTime[0], arriveTime[1]);
 		//Airfare cost
-		//Car rental costs
-		//Miles driven in private vehicle
+		//Car rental costs (check)
+		//Miles driven in private vehicle (check)
+	double carExpenses = calculateCarExpenses(daysNum); //Takes care of rental, private, parking, and taxi
+	printf("Total cost of rentals, private vehicles, parking, and taxis: %f", carExpenses);
 		//Conference/Seminar registration fees
 		//For Each Day:
-			//Parking fee amount
-			//Taxi fees
+			//Parking fee amount (check, see above)
+			//Taxi fees (check, see above)
 			//For Each Meal:
 				//Meal cost
 	
