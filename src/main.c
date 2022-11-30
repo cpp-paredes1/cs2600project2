45f00d5494f15457933949ef797248c5d63ee473
Matthew Finerty <mfinerty@cpp.edu>
2022-11-30 13:47:05 -0800

    Perform unit tests if first argument to program is "test"

--- src/main.c
+++ src/main.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "input.h"
 
-int main() {
-	// Write program template in here
+extern int test();
+
+int main(int argc, char const *argv[]) {
+	// Perform unit tests if first argument is "test"
+	if (argc == 2 && strcmp(argv[1], "test") == 0) {
+		return test();
+	}
+
 	printf("Hello, world!\n");
 	return 0;
 }
