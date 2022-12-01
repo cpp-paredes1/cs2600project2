784950f06317e94de96aa2b90b50cbcf6bb194ff
Kyle Long <kjlong@cpp.edu>
2022-11-30 22:23:42 -0800

    Implemented hotel and meal expenses into main, and added to the allowed expenses

--- include/FoodHotelRegistrationCalc.h
+++ include/FoodHotelRegistrationCalc.h
@@ -3,3 +3,4 @@ double *hotelCost();
 double *calBreakfast(double amount);
 double *calLunch(double amount);
 double *calDinner(double amount);
+double *calMealCost(int numOfDays, double time[2]);
--- src/main.c
+++ src/main.c
@@ -4,12 +4,19 @@
 #include "input.h"
 #include "CarExpenseCalc.h"
 #include "PlaneExpenseCalc.h"
+#include "FoodHotelRegistrationCalc.h"
+
 extern int test();
 int main(int argc, char const *argv[]) {
 	// Perform unit tests if first argument is "test"
 	if (argc == 2 && strcmp(argv[1], "test") == 0) {
 		return test();
 	}
+	//constants for meal expenses
+	const double BREAKFASTALLOWED = 9.0;
+	const double LUNCHALLOWED = 12.0;
+	const double DINNERALLOWED = 16.0;
+	const double HOTELALLOWED = 90.0;
 	//Ask for:
 		//# of days (check)
 	int daysNum = integerInputPositive("Input number of days spent on trip: ", 0);
@@ -19,7 +26,7 @@ int main(int argc, char const *argv[]) {
 	printf("Departure time: %002d:%002d\n", departTime[0], departTime[1]);
 		//Time of arrival (check)
 	int *arriveTime = timeInput("Enter the time of your arrival (XX:XX AM/PM): ");
-	printf("Departure time: %002d:%002d\n\n", arriveTime[0], arriveTime[1]);
+	printf("Arrival time: %002d:%002d\n\n", arriveTime[0], arriveTime[1]);
 		//Airfare cost (check)
 		//Car rental costs (check)
 		//Miles driven in private vehicle (check)
@@ -27,9 +34,21 @@ int main(int argc, char const *argv[]) {
 	double carExpenses = calculateCarExpenses(daysNum); //Takes care of rental, private, parking, and taxi
 	printf("Total cost of rentals, private vehicles, parking, and taxis: %f\n", carExpenses);
 		//Conference/Seminar registration fees
+	double regExpenses = calRegCost();
+	printf("Conference and Seminar registration fees: %f\n", regExpenses);
+		//Hotel Fees
+	double *tempHotelExpenses = hotelCost();
+	double hotelExpenses = tempHotelExpenses[0] + tempHotelExpenses[1];
+	printf("\nHotel expenses: %f\n", hotelExpenses);
 		//For Each Day:
 			//Parking fee amount (check, see above)
 			//Taxi fees (check, see above)
+	int timesForMeals[2];
+	timesForMeals[0] = departTime[0];
+	timesForMeals[1] = arriveTime[0];
+	double *tempMealExpenses = calMealCost(daysNum, timesForMeals);
+	double mealExpenses = tempMealExpenses[0] + tempMealExpenses[1];
+	printf("Meal expenses: %f\n", mealExpenses);
 			//For Each Meal:
 				//Meal cost
 	
@@ -38,10 +57,31 @@ int main(int argc, char const *argv[]) {
 	//Output:
 		//Total expenses incurred
 	double incurredExpenses = 0;
-	incurredExpenses += carExpenses + planeExpenses;
+	incurredExpenses += carExpenses + planeExpenses + regExpenses + hotelExpenses + mealExpenses;
 	printf("Total expenses incurred: %f\n", incurredExpenses);
 		//Total allowed expenses
 	double allowedExpenses = 0;
+	
+	if (departTime[0] < 7) {
+		allowedExpenses += BREAKFASTALLOWED;
+	}
+	else if (departTime[0] < 12) {
+		allowedExpenses += BREAKFASTALLOWED + LUNCHALLOWED;
+	}
+	else if (departTime[0] < 18) {
+		allowedExpenses += BREAKFASTALLOWED + LUNCHALLOWED + DINNERALLOWED;
+	} //end if
+	
+	if (arriveTime[0] > 8 && arriveTime[1] > 0) {
+		allowedExpenses += BREAKFASTALLOWED;
+	}
+	else if (arriveTime[0] > 13 && arriveTime[1] > 0) {
+		allowedExpenses += BREAKFASTALLOWED + LUNCHALLOWED;
+	}
+	else if (arriveTime[0] > 19 && arriveTime[1] > 0) {
+		allowedExpenses += BREAKFASTALLOWED + LUNCHALLOWED + DINNERALLOWED;
+	} //end if
+	allowedExpenses += HOTELALLOWED * daysNum;
 	allowedExpenses += calculateCarExpensesCovered() + planeExpenses;
 	printf("Total allowed expenses: %f\n", allowedExpenses);
 		//If allowed < incurred
